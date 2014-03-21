472fda4d2fda32bce3dd35393cca6ef06add5ce4
silps <silver.kuusik@gmail.com>
2014-03-21 15:27:52 +0100

    public/js/sumorobot.js added example xml

--- public/js/sumorobot.js
+++ public/js/sumorobot.js
@@ -1,32 +1,69 @@
+var line = 
+	"<xml>" +
+		"<block type='controls_if' x='25' y='25'>" +
+			"<value name='IF0'>" +
+		      "<block type='sumorobot_line'>" +
+		        "<field name='LINE'>MIDDLE</field>" +
+		      "</block>" +
+		    "</value>" +
+			"<statement name='DO0'>" +
+				"<block type='sumorobot_move'>" +
+					"<field name='MOVE'>STOP</field>" +
+				"</block>" +
+			"</statement>" +
+		"</block>" +
+	"</xml>";
+
+var enemy = 
+	"<xml>" +
+		"<block type='controls_if' x='25' y='25'>" +
+			"<value name='IF0'>" +
+		      "<block type='sumorobot_enemy'>" +
+		        "<field name='ENEMY'>FRONT</field>" +
+		      "</block>" +
+		    "</value>" +
+			"<statement name='DO0'>" +
+				"<block type='sumorobot_move'>" +
+					"<field name='MOVE'>FORWARD</field>" +
+				"</block>" +
+			"</statement>" +
+		"</block>" +
+	"</xml>";
+
 /* When code has changed */
 var onCodeChanged = function onCodeChanged() {
 	$('#arduino-code').val(Blockly.Arduino.workspaceToCode());
-}
+};
+
+/* Example code snippets */
+var showExample = function showExample(exampleXML) {
+	Blockly.mainWorkspace.clear();
+	var xml = Blockly.Xml.textToDom(exampleXML);
+	Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+};
 
 /* Send the code */
-function sendCode() {
+var sendCode = function sendCode() {
 	socket.emit('send-sumorobot-code', Blockly.Arduino.workspaceToCode());
-}
+};
 
 /* Get a users code */
-function getCode(userEmail) {
+var getCode = function getCode(userEmail) {
 	socket.emit('get-code', 'sumorobot', userEmail);
-}
+};
 
 /* Show the code */
-function showCode() {
+var showCode = function showCode() {
 	alert(Blockly.Arduino.workspaceToCode());
-}
+};
 
 /* When DOM has been loaded */
 window.onload = function() {
 	// initialize blockly
 	Blockly.inject(document.getElementById('blockly-div'), {
-		scrollbars: false,
 		trashcan: true,
 		path: '/js/blockly/',
 		toolbox: document.getElementById('toolbox')
 	});
 	Blockly.addChangeListener(onCodeChanged);
-	Blockly.Trashcan
 }
