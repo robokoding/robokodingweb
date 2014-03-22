1190a2f69570fae8737d2fad2c7d0537a814d2f7
silps <silver.kuusik@gmail.com>
2014-03-22 21:00:29 +0100

    app.js added sockets

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 #!/bin/env node
 
 /* Add modules */
-var express = require('express'),
-	app = express();
+var fs = require('fs'),
+	express = require('express'),
+	socketio = require('socket.io'),
+	exec = require('child_process').exec;
 
 /* Set app properties */
+var app = express();
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
@@ -38,7 +41,46 @@ app.get('/sumorobot', function(req, res) {
 	res.render('sumorobot');
 });
 
-/* Start the app and sockets */
+/* Start the express server */
 var server = app.listen(process.env.PORT, function() {
 	console.log("INFO", "express server listening on port:", process.env.PORT);
+});
+
+/* Start socket.io */
+io = socketio.listen(server);
+console.log("INFO", "socket.io started");
+
+/* User initiated socket connection */
+io.sockets.on('connection', function(socket) {
+	/* User connected to socketio */
+	console.log("INFO", "socket connection established");
+
+	/* User asks for someones code */
+	socket.on('get-sumorobot-code', function(user) {
+		console.log("INFO", "get :", user);
+	});
+
+	/* User send sumorobot code */
+	socket.on('send-sumorobot-code', function(code) {
+		console.log("INFO", "received sumorobot code:", code);
+		/* Write the program to the file */
+		fs.writeFile("public/compiler/main.ino", code, function(err) {
+			if (err) socket.emit('sumorobot-message', "upload failed");
+			else console.log("INFO", "sumorobot code was saved");
+		});
+		console.log("INFO", "compiling sumorobt code");
+		/* Compile the program and upload it */
+		var child = exec("cd public/compiler && make all && make upload",
+			function (error, stdout, stderr) {
+				console.log("INFO", "stdout:", stdout);
+				console.log("INFO", "stderr:", stderr);
+				socket.emit('sumorobot-message', "upload successful");
+			}
+		);
+	});
+
+	/* User disconnected from socket */
+	socket.on('disconnect', function() {
+		console.log("INFO", "socket connection ended");
+	});
 });
