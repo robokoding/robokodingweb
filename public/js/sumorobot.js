e5d0147ecbfb1606ccb6c3939592a19ffbee48d7
silps <silver.kuusik@gmail.com>
2014-05-05 17:11:41 +0200

    public/js/sumorobot.js udpated code examples

--- public/js/sumorobot.js
+++ public/js/sumorobot.js
@@ -1,35 +1,38 @@
-var line = 
+/* Line stopping example */
+var lineExample =
 	"<xml>" +
 		"<block type='controls_if' x='25' y='25'>" +
 			"<value name='IF0'>" +
-		      "<block type='sumorobot_line'>" +
-		        "<field name='LINE'>MIDDLE</field>" +
-		      "</block>" +
-		    "</value>" +
+				"<block type='sumorobot_line'>" +
+					"<field name='LINE'>LINE_MIDDLE</field>" +
+				"</block>" +
+			"</value>" +
 			"<statement name='DO0'>" +
 				"<block type='sumorobot_move'>" +
-					"<field name='MOVE'>STOP</field>" +
+					"<field name='MOVE'>stop</field>" +
 				"</block>" +
 			"</statement>" +
 		"</block>" +
 	"</xml>";
 
-var enemy = 
+/* Enemy attacking example */
+var enemyExample =
 	"<xml>" +
 		"<block type='controls_if' x='25' y='25'>" +
 			"<value name='IF0'>" +
-		      "<block type='sumorobot_enemy'>" +
-		        "<field name='ENEMY'>FRONT</field>" +
-		      "</block>" +
-		    "</value>" +
+				"<block type='sumorobot_enemy'>" +
+					"<field name='ENEMY'>ENEMY_FRONT</field>" +
+				"</block>" +
+			"</value>" +
 			"<statement name='DO0'>" +
 				"<block type='sumorobot_move'>" +
-					"<field name='MOVE'>FORWARD</field>" +
+					"<field name='MOVE'>forward</field>" +
 				"</block>" +
 			"</statement>" +
 		"</block>" +
 	"</xml>";
 
+/* The socket object */
 var socket = null;
 
 /* When code has changed */
@@ -81,3 +84,32 @@ window.onload = function() {
 		alert(message);
 	});
 }
+
+/* When keydown on document */
+$(document).keydown(function(e) {
+	var tag = e.target.tagName.toLowerCase();
+	/* Check the key code */
+	switch(e.which) {
+		case 38:
+			if (tag != 'input' && tag != 'textarea') /* UP */;
+			break;
+		case 40:
+			if (tag != 'input' && tag != 'textarea') /* DOWN */;
+			break;
+		case 37:
+			if (tag != 'input' && tag != 'textarea') /* LEFT */;
+			break;
+		case 39:
+			if (tag != 'input' && tag != 'textarea') /* RIGHT */;
+			break;
+		case 69:
+			if (tag != 'input' && tag != 'textarea') showExample(enemyExample);
+			break;
+		case 76:
+			if (tag != 'input' && tag != 'textarea') showExample(lineExample);
+			break;
+		case 85:
+			if (tag != 'input' && tag != 'textarea') sendCode();
+			break;
+	}
+});
